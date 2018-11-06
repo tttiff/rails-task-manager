ef9e885c6614be3325c89f242e11ac0aec69e604
tttiff <charles.tiffany.r@gmail.com>
2018-11-06 12:03:24 +0000

    deleted and redded controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   get 'tasks/#id', to: 'tasks#show'
 
   get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
 
-  post 'tasks', to:
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to:'tasks#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
